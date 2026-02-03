d46298e978cd793af70ac8fbd5c65344096bb206
Caden Warren <143781126+cw-0@users.noreply.github.com>
2026-02-02 21:51:35 -0500

    Delete nvim directory

--- nvim/lua/custom/keybinds.lua
+++
@@ -1,6 +0,0 @@
--- Custom keybindings
-local keymap = vim.keymap.set
-local opts = { noremap = true, silent = true }
-
--- Exit Insert mode with Ctrl + alt + {any key}
-keymap('i', '<C-M>', '<Esc>', opts)
